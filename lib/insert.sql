c5e5f3f8d772f737a29ce49b26c88cfbfeab4496
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-16 21:42:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'Male', 'Brown', 'Nice', 'Yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 6, 'Male', 'Brown', 'Wild', 'Yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 8, 'Female', 'Brown', 'Nice', 'Yes');
